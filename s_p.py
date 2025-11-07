1df220a9ff37af4e906fea8d4d30e2fb9b5acb18
Sergei Iakukhin <ser.yakukhin.inbox@gmail.com>
2025-11-07 22:04:11 +0300

    add function sum

--- s_p.py
+++ s_p.py
@@ -1,1 +1,8 @@
 """Module"""
+def find_sum(spisok: list[int]) -> int:
+    """Function"""
+    return sum(spisok)
+
+if __name__ == '__main__':
+    assert find_sum([2,4,6]) == 12
+    print('Test passed')
